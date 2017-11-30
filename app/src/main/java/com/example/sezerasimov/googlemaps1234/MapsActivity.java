02b3723d0329d92d9ab2a41eeb5b9514a9bfb854
sasimov <sezer.asimov@googlemail.com>
2017-11-30 13:10:55 +0100

    abc

--- app/src/main/java/com/example/sezerasimov/googlemaps1234/MapsActivity.java
+++ app/src/main/java/com/example/sezerasimov/googlemaps1234/MapsActivity.java
@@ -38,7 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camer
+        // Add a marker in Sydney and move the camera
+        //hi
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
